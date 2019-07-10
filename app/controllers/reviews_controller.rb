87bfa20d47c4b43d497f665db2c9f5a26029f18a
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-07-10 18:48:31 +0900

    create new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,26 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
   end
   
+  def create
+    @review = Review.new(review_params)
+    
+    if @review.save
+      redirect_to root_path
+    else
+      redirect_to new_item_path
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+    :name,
+    :text,
+    :longitude,
+    :latitude).merge(user_id: current_user.id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_111832) do
+ActiveRecord::Schema.define(version: 2019_07_10_063144) do
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_07_08_111832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
