0fab31cabe0198220be76b294a460d05c0dd8878
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-10-21 20:26:00 +0900

    readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @user = User.find(current_user.id)
+    @follow = Relationship.where(user_id: @user.id)
+    @follower = Relationship.where(follow_id: @user.id)
     @total = 0
     @user.reviews.each do |review|
       if review.likes_count.present?
